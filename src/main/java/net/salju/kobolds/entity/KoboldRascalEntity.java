b60d3b712d65bf15207ecd3965c382a72e8a3ca2
Salju <61861078+Jusey1@users.noreply.github.com>
2023-05-25 01:26:23 -0400

    Add files via upload

--- src/main/java/net/salju/kobolds/entity/KoboldRascalEntity.java
+++ src/main/java/net/salju/kobolds/entity/KoboldRascalEntity.java
@@ -93,7 +93,7 @@ public class KoboldRascalEntity extends AbstractKoboldEntity {
 			int x = pos.getX();
 			int y = pos.getY();
 			int z = pos.getZ();
-			return (!world.isClientSide() && world.isEmptyBlock(new BlockPos(x, y, z)) && world.getBlockState(new BlockPos(x, y, z)).getLightEmission(world, new BlockPos(x, y, z)) <= 4
+			return (y < 56 && !world.isClientSide() && world.isEmptyBlock(new BlockPos(x, y, z)) && world.getBlockState(new BlockPos(x, y, z)).getLightEmission(world, new BlockPos(x, y, z)) <= 2
 					&& !world.getEntitiesOfClass(KoboldEntity.class, AABB.ofSize(new Vec3(x, y, z), 64, 64, 64), e -> true).isEmpty()
 					&& !(!world.getEntitiesOfClass(KoboldRascalEntity.class, AABB.ofSize(new Vec3(x, y, z), 128, 128, 128), e -> true).isEmpty()));
 		});
